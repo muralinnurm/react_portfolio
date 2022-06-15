5e9ab359babcbec4e7d47e6c99d0be4076a10c3a
MorsaliN NuR Mahim <98252719+muralinnurm@users.noreply.github.com>
2022-06-15 21:22:52 +0600

    about component created

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,13 +1,52 @@
-import React from 'react';
-import './about.css';
+import React from "react";
+import "./about.css";
+import myImage from "../../assets/about-img.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 function About() {
   return (
     <section id="about">
-      about
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about-container">
+        <div className="about-me">
+          <div className="about-me-image">
+            <img src={myImage} alt="My Image" />
+          </div>
+        </div>
+
+        <div className="about-content">
+          <div className="about-cards">
+            <article className="about-card">
+              <FaAward className="about-icon" />
+              <h5>Experience</h5>
+              <small>2+ years Working</small>
+            </article>
+            <article className="about-card">
+              <FiUsers className="about-icon" />
+              <h5>Clients</h5>
+              <small>3+ Client Worldwide</small>
+            </article>
+            <article className="about-card">
+              <VscFolderLibrary className="about-icon" />
+              <h5>Projects</h5>
+              <small>20+ Completed</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo sequi
+            ipsa, beatae eveniet reiciendis vero excepturi sapiente quos ducimus
+            molestiae! Ducimus molestiae reprehenderit dolorem soluta harum
+            commodi. Nemo, animi enim?
+          </p>
+
+          <a href="#contact" className="btn btn-primery">Let's Talk</a>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default About
-
+export default About;
