a789b027bfdc30f6c69e272de8764bf7e6471d74
MorsaliN NuR Mahim <98252719+muralinnurm@users.noreply.github.com>
2022-06-16 17:47:15 +0600

    skill component created

--- src/components/experince/Experince.js
+++ src/components/experince/Experince.js
@@ -1,10 +1,101 @@
 import React from 'react';
 import './experince.css';
+import { BsPatchCheckFill } from 'react-icons/bs'
 
 export default function Experince() {
   return (
     <section id='experince'>
-      Experince
+      <h5>What Skill I Have</h5>
+      <h2>My Experince</h2>
+      <div className="container experince-container">
+        <div className="experince-programming">
+          <h3>Frontend Deevelopment</h3>
+          <div className="experince-content">
+            <article className='experince-details'>
+              <BsPatchCheckFill className='experince-details-icon'/>
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+
+            </article>
+            <article className='experince-details'>
+              <BsPatchCheckFill className='experince-details-icon'/>
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experince-details'>
+              <BsPatchCheckFill className='experince-details-icon'/>
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Experienced</small></div>
+            </article>
+            <article className='experince-details'>
+              <BsPatchCheckFill className='experince-details-icon'/>
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experince-details'>
+              <BsPatchCheckFill className='experince-details-icon'/>
+              <div>
+                <h4>Tailwind CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+
+            </article>
+            <article className='experince-details'>
+              <BsPatchCheckFill className='experince-details-icon'/>
+              <div>
+                <h4>React JS</h4>
+                <small className='text-light'>Learning</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experince-other">
+          <h3>Other Experince</h3>
+          <div className="experince-content">
+            <article className='experince-details'>
+              <BsPatchCheckFill className='experince-details-icon'/>
+              <div>
+                <h4>Wordpress</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experince-details'>
+              <BsPatchCheckFill className='experince-details-icon'/>
+              <div>
+                <h4>Woo Commerce</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experince-details'>
+              <BsPatchCheckFill className='experince-details-icon'/>
+              <div>
+                <h4>Wix</h4>
+                <small className='text-light'>Intermediate</small></div>
+            </article>
+            <article className='experince-details'>
+              <BsPatchCheckFill className='experince-details-icon'/>
+              <div>
+                <h4>Shopify</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experince-details'>
+              <BsPatchCheckFill className='experince-details-icon'/>
+              <div>
+                <h4>Joomla</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -2,8 +2,19 @@ import React from 'react'
 import "./services.css"
 export default function Services() {
   return (
-    <section id='service'>
-      Services
+    <section id='services'>
+       <h5>What I Offer</h5>
+       <h2>Services</h2>
+      <div className="container services-container">
+        <article className='service'>
+          <div className='service-head'>
+            <h3>Web Design</h3>
+          </div>
+          <ul className='sservice-list'>
+            <li></li>
+          </ul>
+        </article>
+      </div>
     </section>
   )
 }
